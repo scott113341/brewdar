dd86a480766ce438c59f61db4583955b12e45a97
Scott Hardy <scott.the.hardy@gmail.com>
2014-10-03 15:35:30 -0700

    model fixes

--- brewdar/users/models.py
+++ brewdar/users/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class User(models.Model):
-    email = models.EmailField
+    email = models.EmailField()
 
 
 class Device(models.Model):
-    device_id = models.CharField
-    name = models.CharField
-    verification_token = models.CharField
-    verified = models.BooleanField
-    authentication_token = models.CharField
-    authenticated = models.BooleanField
+    device_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    verification_token = models.CharField(max_length=16)
+    verified = models.BooleanField()
+    authentication_token = models.CharField(max_length=16)
+    authenticated = models.BooleanField()
     user = models.ForeignKey(User)
