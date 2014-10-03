6711780113201a26a76a221ad0d94585da2a6014
Scott Hardy <scott.the.hardy@gmail.com>
2014-10-03 16:09:06 -0700

    more model stuff

--- brewdar/users/admin.py
+++ brewdar/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models import User, Device
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email']
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'device_id', 'name', 'verification_token',
+                    'verified', 'authentication_token', 'authenticated', 'user']
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Device, DeviceAdmin)
--- brewdar/users/models.py
+++ brewdar/users/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class User(models.Model):
     email = models.EmailField()
 
+    def __unicode__(self):
+        return self.email
+
 
 class Device(models.Model):
     device_id = models.CharField(max_length=100)
@@ -13,3 +16,20 @@ class Device(models.Model):
     authentication_token = models.CharField(max_length=16)
     authenticated = models.BooleanField()
     user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.user.email + ", " + self.device_id
+
+    def verify(self, verification_token):
+        if self.verification_token == verification_token:
+            self.verified = True
+            self.save()
+            return True
+        return False
+
+    def authenticate(self, authentication_token):
+        if self.authentication_token == authentication_token:
+            self.authenticated = True
+            self.save()
+            return True
+        return False
